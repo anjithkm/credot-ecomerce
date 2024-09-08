cc1cb13d793c783da1d54e0e32ee555f8b5ba825
anjithkm <anjithkm10@gmail.com>
2024-09-08 23:24:43 +0530

    logout button in login page solved

--- frontend/src/component/common/header.tsx
+++ frontend/src/component/common/header.tsx
@@ -9,8 +9,9 @@ import { useAppDispatch, useAppSelector } from '@/store/hooks';
 const Header : React.FC = () => {
 
   const dispatch = useAppDispatch();
-  const navigate = useNavigate()
-  const { auth } = useAppSelector(store => store.auth);
+  const navigate = useNavigate();
+
+  const { auth,isAuthenticated } = useAppSelector(store => store.auth);
 
   const HandleLogout=()=>{
     dispatch(logout())
@@ -20,13 +21,17 @@ const Header : React.FC = () => {
      <header className="header">
       <div className='header-container'>
         <img className='logo' src='/assets/images/logo.png' onClick={()=>{navigate(`/${auth?.user}/home`)}} />
-        <div className='header-extra'>
-          <Button title="Profile" className='rounded header-button' icon={<img  src='/assets/icons/user.svg' alt=""/>} />
-           <div className='vertical-seperator header-extra-seperator-margin'/>
-          <Button title="Cart"  onClick={()=>{navigate(`/${auth?.user}/cart`)}} className='rounded header-button' icon={<img  src='/assets/icons/bag-happy.svg' alt=""/>} />
-          <div className='vertical-seperator header-extra-seperator-margin'/>
-          <Button title="log out" onClick={HandleLogout} className='rounded header-button' icon={<img  src='/assets/icons/logout.svg' alt=""/>} />
-        </div>
+        {
+          isAuthenticated && (
+            <div className='header-extra'>
+              <Button title="Profile" className='rounded header-button' icon={<img  src='/assets/icons/user.svg' alt=""/>} />
+               <div className='vertical-seperator header-extra-seperator-margin'/>
+              <Button title="Cart"  onClick={()=>{navigate(`/${auth?.user}/cart`)}} className='rounded header-button' icon={<img  src='/assets/icons/bag-happy.svg' alt=""/>} />
+              <div className='vertical-seperator header-extra-seperator-margin'/>
+              <Button title="log out" onClick={HandleLogout} className='rounded header-button' icon={<img  src='/assets/icons/logout.svg' alt=""/>} />
+            </div>
+            )
+          }
       </div>
     </header>
     )
